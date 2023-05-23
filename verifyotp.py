626eb0364059a41cc2314b315b5133006a810c99
hamnizeen <hamnizeen@gmail.com>
2023-05-23 21:23:28 +0530

    final commit

--- verifyotp.py
+++ verifyotp.py
@@ -1,5 +1,6 @@
 from tkinter import *
 import subprocess
+from PIL import Image, ImageTk
 
 def check_file():
     
@@ -21,21 +22,20 @@ root = Tk()
 root.title("OTP Verification")
 root.geometry("1350x900")
 
+image = Image.open("plane.jpg")
+image = image.resize((1350, 900), Image.LANCZOS)
+photo = ImageTk.PhotoImage(image)
 
 canvas = Canvas(root, width=1350, height=900)
-bg_image = PhotoImage(file="image.png")
-canvas.create_image(0, 0, anchor=NW, image=bg_image)
-canvas.grid(row=0, column=0)
+canvas.place(relx=0.5, rely=0.5, anchor="center")
 
-
-frame = Frame(root)
-frame.place(relx=0.5, rely=0.5, anchor="center")
-
-
+canvas.create_image(0, 0, anchor="nw", image=photo)
 font_style = ("TkDefaultFont", 13, "bold")
 button_width = 15
 button_height = 2
 
+frame = Frame(root)
+frame.place(relx=0.5, rely=0.5, anchor="center")
 
 input_label = Label(frame, text="Enter the OTP Sent:", font=font_style)
 input_label.grid(row=0, column=0, padx=5, pady=5)
